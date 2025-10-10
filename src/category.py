8df16847a5d2279e5f2c580aab789e862cd7233d
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-10-11 02:00:47 +0500

    add category.py and product.py

---
+++ src/category.py
@@ -0,0 +1,18 @@
+from src.product import Product
+
+
+class Category(Product):
+    name: str
+    description: str
+    products: list
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        self.product_count = len(products)
+
+        Category.category_count += 1
