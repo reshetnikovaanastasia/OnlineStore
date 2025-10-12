f937a1b1066f8f569f0f2509e0d1699786fedc3c
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-10-12 18:37:34 +0500

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture
+def first_product():
+    return Product("Samsung Galaxy S23 Ultra",
+                   "256GB, Серый цвет, 200MP камера",
+                   180000.0,
+                   5)
+
+
+@pytest.fixture()
+def first_category():
+    return Category("Смартфоны",
+                    "Смартфоны, как средство не только коммуникации, "
+                    "но и получения дополнительных функций для удобства жизни",
+                    [Product("Iphone 15",
+                             "512GB, Gray space",
+                             210000.0,
+                             8), Product("Samsung Galaxy S23 Ultra",
+                                         "256GB, Серый цвет, 200MP камера",
+                                         180000.0,
+                                         5)])
+
+
+@pytest.fixture()
+def second_category():
+    return Category("Телевизоры",
+                    "Современный телевизор, который позволяет наслаждаться просмотром, "
+                    "станет вашим другом и помощником",
+                    [Product("55\" QLED 4K",
+                             "Фоновая подсветка",
+                             123000.0,
+                             7)])
---
+++ tests/test_product.py
@@ -0,0 +1,5 @@
+def test_product_init(first_product):
+    assert first_product.name == "Samsung Galaxy S23 Ultra"
+    assert first_product.description == "256GB, Серый цвет, 200MP камера"
+    assert first_product.price == 180000.0
+    assert first_product.quantity == 5
