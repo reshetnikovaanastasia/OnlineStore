c15f78793388a777df7b8780c2ce6ecf9990881f
анастасия решетникова <reshetnikovaanastasia04@gmail.com>
2025-10-14 21:49:38 +0500

    add funcs in src

--- src/category.py
+++ src/category.py
@@ -9,11 +9,22 @@ class Category():
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
 
     def add_product(self, product):
-        self.products.append(product)
+        """Добавление продуктов в категорию"""
+        self.__products.append(product)
         Category.product_count += 1
+
+    @property
+    def products(self):
+        """Возвращает список продуктов"""
+        # for pr in self.__products:
+        #     return f'{Product.name}, {Product.price} руб. Остаток: {Product.quantity} шт.'
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
